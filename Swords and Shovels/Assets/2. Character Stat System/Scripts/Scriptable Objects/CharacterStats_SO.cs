baeb9f28e8c4615efc368230f37d00f25a224e27
DESKTOP-IGHA7RH\Adrian Gamarra <adriatogi@gmail.com>
2020-06-18 14:18:20 -0700

    Creating a Character Stat System in Unity

---
+++ Swords and Shovels/Assets/2. Character Stat System/Scripts/Scriptable Objects/CharacterStats_SO.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="NewStats", menuName ="Character/Stats", order = 1)]
+
+public class CharacterStats_SO : ScriptableObject
+{
+    #region Fields
+
+    public bool setManually = false;
+    public bool saveDataOnClose = false;
+
+    public int maxHealth = 0;
+    public int currentHealth = 0;
+
+    public int baseDamage = 0;
+    public int currentDamage = 0;
+
+    public float baseResistance = 0f;
+    public float currentResistance = 0f;
+
+    public float currentEmcumbrance = 0f;
+
+    public int charExperience = 0;
+    public int charLevel = 0;
+
+
+    #endregion
+}
