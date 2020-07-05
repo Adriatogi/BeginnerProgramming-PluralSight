a04f096580f306479d4e47dd2c447f6c43bf5b2d
DESKTOP-IGHA7RH\Adrian Gamarra <adriatogi@gmail.com>
2020-07-05 12:07:38 -0700

    Scene set-up

---
+++ Swords and Shovels/Assets/3. Game Manager/Scripts/GameManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // What level the game is currently in
+    // load and unload game levels
+    // keep track of the game stats
+    // generate other persistent systems
+}
